e5bb0f7232abc1a22e2eae1039497745d63f347e
filipe-oliv95 <filipec.oliveira95@gmail.com>
2024-11-07 19:38:25 -0300

    docs(swagger): configurar OpenAPI no arquivo principal da aplicação
    - Aplica as anotações do Swagger no arquivo principal para gerar a documentação da API.
    - Define informações gerais da API, como título, descrição, versão e contato para a API.
    - Melhora a documentação e a acessibilidade dos endpoints da API.

--- src/main/java/com/restaurante/marmitas/MarmitasApplication.java
+++ src/main/java/com/restaurante/marmitas/MarmitasApplication.java
@@ -4,6 +4,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+	    info = @Info(
+	        title = "Restaurante Marmitas - API de Produtos",
+	        description = "API REST para gerenciamento de um restaurante.",
+	        version = "v1",
+	        contact = @Contact(
+	            name = "Filipe Oliveira",
+	            email = "filipec.oliveira95@gmail.com",
+	            url = "https://github.com/filipe-oliv95"
+	        )
+	    ),
+	    externalDocs = @ExternalDocumentation(
+	        description = "Documentação completa da API",
+	        url = "https://github.com/filipe-oliv95/sistema-restaurante"
+	    )
+	)
 @SpringBootApplication
 @EnableJpaAuditing
 public class MarmitasApplication {
