19f9688c0bf3de6fc161e7697cbd88696c295dd8
filipe-oliv95 <filipec.oliveira95@gmail.com>
2024-11-07 19:08:49 -0300

    feat(logs): adiciona logs no método createProduto e configura logs no application.yml
    - Adiciona logs no ProdutoService para detalhar o processo de criação, incluindo mensagens de aviso.
    - Configura o application.yml para direcionar logs para o arquivo  e definir o nível de log para DEBUG na aplicação, melhorando o monitoramento e a rastreabilidade do fluxo de criação de produtos.

--- src/main/java/com/restaurante/marmitas/service/impl/ProdutoService.java
+++ src/main/java/com/restaurante/marmitas/service/impl/ProdutoService.java
@@ -11,7 +11,9 @@ import com.restaurante.marmitas.repository.ProdutoRepository;
 import com.restaurante.marmitas.service.IProdutoService;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class ProdutoService implements IProdutoService{
@@ -20,12 +22,15 @@ public class ProdutoService implements IProdutoService{
 	
 	@Override
 	public void createProduto(ProdutoRequestDto produtoRequestDto) {
+		log.debug("Iniciando criação do produto com dados: {}", produtoRequestDto);
 		Produto produto = ProdutoMapper.mapToProduto(produtoRequestDto, new Produto());
 		produtoRepository.findByNome(produtoRequestDto.getNome())
 				.ifPresent(produtoExistente -> {
+					log.warn("Produto já existe com o nome: {}", produtoRequestDto.getNome());
 		            throw new ProductAlreadyExistsException(ProdutoConstants.NOME_ALREADY_EXIST + produtoRequestDto.getNome());
 		        });
 		produtoRepository.save(produto);
+		log.info("Produto salvo com sucesso: {}", produto.getNome());
 	}
 
 }
